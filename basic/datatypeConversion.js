c4571384012df2cef0ce989d4667ecad8c7e019c
bankuiscoding <151135601+bankuiscoding@users.noreply.github.com>
2025-08-19 15:37:27 +0000

    conversion of data type

---
+++ basic/datatypeConversion.js
@@ -0,0 +1,18 @@
+let score = "33avc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber );
+
+//"33"=>33
+//"33abc"=>Nan
+//true=>1;false=>0
+//undefined =>Nan
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+//""=>false
+//"ANkit"=>true
