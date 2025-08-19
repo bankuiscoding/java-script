a8c3f665fb35ec442058d429d5cc7e8c246e70fe
bankuiscoding <151135601+bankuiscoding@users.noreply.github.com>
2025-08-19 16:31:00 +0000

    operation

--- basic/datatypeConversion.js
+++ basic/datatypeConversion.js
@@ -1,4 +1,4 @@
-let score = "33avc"
+/*let score = "33avc"
 console.log(typeof score);
 console.log(typeof(score));
 
@@ -16,3 +16,46 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
 //""=>false
 //"ANkit"=>true
+*/
+
+
+
+//****************** Operations************
+
+
+let value =3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+
+//let str1 = "HEllo"
+//let str2 = " ankit"
+//let str3 = str1+str2
+//console.log(str3)
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+console.log(+true);
+//console.log(++"");
+
+//let num1,num2,num3
+
+
+//num1= num2=num3=2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
