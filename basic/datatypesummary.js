8419922cd5c612077c127f57dc023d7dcb3f94c0
bankuiscoding <151135601+bankuiscoding@users.noreply.github.com>
2025-08-20 06:50:12 +0000

    datatype summary

--- basic/datatypesummary.js
+++ basic/datatypesummary.js
@@ -29,3 +29,26 @@ let myObj={
 const myFunction = function(){
     console.log("Namaste India");
 }
+
+//*****************Memory **************
+
+//memory two types stack and heap
+//stack use for primitive data types-we accept copy in this
+//heap use for non primitive data types
+
+let myGmailname = "ankitarya@gmail.com"
+ let otherGmailname ="ANKIR"
+ console.log(otherGmailname)
+ console.log(myGmailname);
+
+
+ let userOne={
+    email:"ankit",
+
+    age:12,
+ }
+
+ let userTwo= {
+    email:"arya",,
+    age:23
+ }
